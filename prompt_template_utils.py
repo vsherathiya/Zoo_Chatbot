c1ef837d7e8c6cb40e3006ae1d77971ab917bcac
vsherathiya <viral.sherathiya@nexuslinkservices.in>
2023-11-03 14:54:09 +0530

    Revert "Threading Prompt Template"
    This reverts commit 542097b1b951a23dcaefc9b9f61fab5870e6f69b.

--- prompt_template_utils.py
+++ prompt_template_utils.py
@@ -15,7 +15,7 @@ from langchain.prompts import PromptTemplate
 system_prompt = """You are a customer support chatbot speaking to a user in German. Use the context provided to answer user questions in German. The conversation between you and the user should seem human, and your answers should be short , precise and accurate. Additionally, your answers should be in German and directly address the question asked. If you cannot answer a user question based on the context provided, inform the user about this. Do not use other information to answer user questions . Make sure you do not offer any explanations, reasons or suggestions if you do not have an answer from the context provided. If you are asked a question out of context, answer 'I don't know'."""
 
 
-# system_prompt = """You're a customer support bot that is talking to a user, you will use the provided context to answer user questions. Conversation between you and user should be like human and your reply should be always in german language ,Only Language You Should Understand is German dont provide Other Language answer, read the given context before answering questions and think step by step. Your answer should be short, precise, and accurate and in german language. Furthermore, Your answer should be Straight foreword and in german. If you can not answer a user question based on the provided context, inform the user. Do not use any other information for answering user. Make sure that you don't provide any explanation, reason or any suggestion when you don't have user answer from provided context. If Question asked is in English language than your reply should be  "Bitte stellen Sie Ihre Frage iner Sprache"."""
+# system_prompt = """You're a customer support bot that is talking to a user, you will use the provided context to answer user questions. Conversation between you and user should be like human and your reply should be always in german language ,Only Language You Should Understand is German dont provide Other Language answer, read the given context before answering questions and think step by step. Your answer should be short, precise, and accurate and in german language. Furthermore, Your answer should be Straight foreword and in german. If you can not answer a user question based on the provided context, inform the user. Do not use any other information for answering user. Make sure that you don't provide any explanation, reason or any suggestion when you don't have user answer from provided context. If Question asked is in English language than your reply should be  "Bitte stellen Sie Ihre Frage in deutscher Sprache"."""
 
 
 def get_prompt_template(system_prompt=system_prompt, promptTemplate_type=None, history=False):
@@ -25,18 +25,18 @@ def get_prompt_template(system_prompt=system_prompt, promptTemplate_type=None, h
         SYSTEM_PROMPT = B_SYS + system_prompt + E_SYS
         if history:
             instruction = """
-            Context in the following language: German: {history} \n German {context}
-            User question in the following language : German :{question}
-            Answer in the following language: German
+            Context in the following language: German Deutsch {history} \n{context}
+            User question in the following language : German Deutsch {question}
+            Answer in the following language: German Deutsch
             """
 
             prompt_template = B_INST + SYSTEM_PROMPT + instruction + E_INST
             prompt = PromptTemplate(input_variables=["history", "context", "question"], template=prompt_template)
         else:
             instruction = """
-            Context in the following language: German :{context}
-            User question in the following language : German :{question}
-            Answer in the following language: German """
+            Context in the following language: German Deutsch {context}
+            User question in the following language : German Deutsch {question}
+            Answer in the following language: German Deutsch """
 
             prompt_template = B_INST + SYSTEM_PROMPT + instruction + E_INST
             prompt = PromptTemplate(input_variables=["context", "question"], template=prompt_template)
@@ -47,9 +47,9 @@ def get_prompt_template(system_prompt=system_prompt, promptTemplate_type=None, h
                 B_INST
                 + system_prompt
                 + """
-            Context in the following language: German {history} \n German : {context}
-            User question in the following language : German :{question}
-            Answer in the following language: German
+            Context in the following language: German Deutsch {history} \n {context}
+            User question in the following language : German Deutsch {question}
+            Answer in the following language: German Deutsch
             """
                 + E_INST
             )
@@ -60,9 +60,9 @@ def get_prompt_template(system_prompt=system_prompt, promptTemplate_type=None, h
                 + system_prompt
                 + """
             
-            Context in the following language: German :{context}
-            User question in the following language : German : {question}
-            Answer in the following language: German"""
+            Context in the following language: German Deutsch {context}
+            User question in the following language : German Deutsch {question}
+            Answer in the following language: German Deutsch"""
                 + E_INST
             )
             prompt = PromptTemplate(input_variables=["context", "question"], template=prompt_template)
@@ -73,18 +73,18 @@ def get_prompt_template(system_prompt=system_prompt, promptTemplate_type=None, h
                 system_prompt
                 + """
     
-            Context in the following language: German {history} \n German : {context}
-            User question in the following language : German : {question}
-            Answer in the following language: German"""
+            Context in the following language: German Deutsch {history} \n {context}
+            User question in the following language : German Deutsch {question}
+            Answer in the following language: German Deutsch"""
             )
             prompt = PromptTemplate(input_variables=["history", "context", "question"], template=prompt_template)
         else:
             prompt_template = (
                 system_prompt
                 + """    
-          Context in the following language: German :{context}
-            User question in the following language : German : {question}
-            Answer in the following language: German
+          Context in the following language: German Deutsch {context}
+            User question in the following language : German Deutsch {question}
+            Answer in the following language: German Deutsch
             """
             )
             prompt = PromptTemplate(input_variables=["context", "question"],
